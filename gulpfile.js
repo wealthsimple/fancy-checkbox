c0a812539dd07d52291aee04053697c0f27b91e9
Peter Graham <peter@wealthsimple.com>
2015-10-28 14:16:31 -0400

    base64 inline background images

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var cssBase64 = require('gulp-css-base64');
 
 gulp.task('scripts', function() {
   return gulp.src(['./bower_components/iCheck/icheck.js', './src/fancy-checkbox.js'])
@@ -9,4 +11,18 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('sass', function () {
+  return gulp.src('./src/fancy-checkbox.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src'));
+});
+
+gulp.task('inline', function() {
+  return gulp.src('./src/fancy-checkbox.css')
+    .pipe(cssBase64({
+      extensionsAllowed: ['.png']
+    }))
+    .pipe(gulp.dest('./'));
+})
+
+gulp.task('default', ['scripts', 'sass', 'inline']);
